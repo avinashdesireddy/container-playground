8c4cc386733774cce467fbed79e5fbefc1b9e7d7
Avinash Reddy Desireddy <avinashr.desireddy@gmail.com>
2020-02-19 14:43:24 -0600

    fix for new version

--- docker-enterprise/tools/backup/backup.sh
+++ docker-enterprise/tools/backup/backup.sh
@@ -13,76 +13,78 @@ UCP_VERSION="2.1.5"
 DTR_URL="2.2.2.2"
 DTR_VERSION="2.3.3"
 DTR_REPLICA="5007fbad4136"
+BACKUP_BASE_LOCATION="/linuxit/backup/docker/"
 
+ucp-backup() {
+    echo "----------- Docker EE Backup Tool -----------"
+    echo ""
+    echo ""
 
-ucpBackup() {
     # Timeout required to allow user to cancel backup
     echo "Starting UCP backup in 20 seconds."
     echo "This UCP controller will temporarily shut down but an HA UCP will remain up."
     sleep 20
 
     # Health checking for this UCP controller before backup
-    if [ "$(curl -k -s "https://${UCP_URL}/_ping")" != "OK" ]; then
+    UCP_HEALTH=$(curl -k -s "https://${UCP_URL}/_ping")
+    if [ "${UCP_HEALTH}" != "OK" ]; then
         echo "UCP Controller ${UCP_URL} returned bad health. Exiting backup script."
         exit 1
     fi
     echo "UCP health is OK"
 
-    UCP_BACKUP_NAME="ucp-${UCP_VERSION}-backup-$(date +%F-%R).tar"
+    UCP_BACKUP_NAME=${BACKUP_BASE_LOCATION}/"ucp-${UCP_VERSION}-backup-$(date +"%Y%m%d_%H%M%S").tar"
 
-    UCP_ID="$(docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:"${UCP_VERSION}" id 2> /dev/null)"
+    UCP_ID=$(docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock "docker/ucp:${UCP_VERSION}" id 2> /dev/null)
 
     echo "UCP backup starting ..."
     docker run --rm -i --name ucp \
+        --log-driver none \
         -v /var/run/docker.sock:/var/run/docker.sock \
         "docker/ucp:${UCP_VERSION}" backup \
-        --id "${UCP_ID}" \
-        --debug > "${UCP_BACKUP_NAME}"
+        --no-passphrase > "$UCP_BACKUP_NAME"
 
-    if [ ! -s "${UCP_BACKUP_NAME}" ]; then
+    if [ $(/bin/ls -ls $UCP_BACKUP_NAME | awk '{print $6}' | bc) == 0 ]; then
         echo "Backup was unsucessful. Run UCP backup manually."
         exit 1
     fi
 
-    echo "UCP backup complete and saved as ${UCP_BACKUP_NAME}"
+    echo "UCP backup complete and saved as $UCP_BACKUP_NAME"
 }
 
-
-dtrBackup() {
+dtr-backup() {
     # Timeout required to allow UCP to come backup. UCP was restarted in previous step.
     echo "Starting DTR backup in 30 seconds. DTR will continue running during the backup."
     sleep 30
 
     # Health checking DTR replica before backup
-    if [ "$(curl -k -s "https://${DTR_URL}/health" | jq ".Healthy")" != "true" ]; then
+    DTR_HEALTH=$(curl -k -s "https://${DTR_URL}/health" | python -c "import sys, json; print json.load(sys.stdin)['Healthy']")
+    if [ "${DTR_HEALTH}" != "True" ]; then
         echo "DTR Replica ${DTR_URL} returned bad health. Exiting backup script."
         exit 1
     fi
     echo "DTR health is OK"
 
-    DTR_BACKUP_NAME="dtr-${DTR_VERSION}-backup-$(date +%F-%R).tar"
+    DTR_BACKUP_NAME=${BACKUP_BASE_LOCATION}/"dtr-${DTR_VERSION}-backup-$(date +"%Y%m%d_%H%M%S").tar"
 
     echo "DTR backup starting ..."
-    docker run -i --rm docker/dtr:"${DTR_VERSION}" backup \
-      --debug \
-      --ucp-username "${USERNAME}" \
-      --ucp-password "${PASSWORD}" \
-      --ucp-insecure-tls \
-      --existing-replica-id "${DTR_REPLICA}" \
-      --ucp-url "https://${UCP_URL}" > "${DTR_BACKUP_NAME}"
+    docker run --log-driver none -i --rm \
+        "docker/dtr:${DTR_VERSION}" backup \
+        --ucp-url "https://${UCP_URL}" \
+        --ucp-insecure-tls \
+        --ucp-username ${USERNAME} \
+        --ucp-password "${PASSWORD}" \
+        --existing-replica-id "${DTR_REPLICA}" --debug > "$DTR_BACKUP_NAME"
 
-    if [ ! -s "${DTR_BACKUP_NAME}" ]; then
+    if [ $(/bin/ls -ls $DTR_BACKUP_NAME | awk '{print $6}' | bc) == 0 ]; then
         echo "Backup was unsucessful. Run DTR backup manually."
         exit 1
     fi
 
-    echo "DTR backup complete and saved as ${DTR_BACKUP_NAME}"
+    echo "DTR backup complete and saved as $DTR_BACKUP_NAME"
 }
 
 #Entrypoint for program
-echo "----------- Docker EE Backup Tool -----------"
-echo ""
-echo ""
-ucpBackup
-dtrBackup
+ucp-backup
+dtr-backup
 echo "UCP and DTR backups are complete. Remember to test the backups with a UCP & DTR restore from time to time."
